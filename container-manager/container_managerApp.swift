033880cf82449f14a76e8dfafc2003d4d1de9154
Andy Bold <andy.bold@me.com>
2025-12-30 17:42:34 +0000

    Initial Commit

---
+++ container-manager/container_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  container_managerApp.swift
+//  container-manager
+//
+//  Created by Andrew Bold on 30/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct container_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
